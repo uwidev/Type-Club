88550b01b7e76f1196fd93b17f56408cef337aef
Annie Zhao <anniezhao107@gmail.com>
2019-10-26 14:46:45 -0700

    added to autoload the GlobalVariables script

---
+++ Scripts/GlobalVariables.gd
@@ -0,0 +1,18 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var can_type: bool = false
+var health: int = 100
+var enemies: Array = []
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
