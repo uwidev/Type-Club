eade7d8b38f1b7946de4ca230606460e5f1055b3
usaradark <gamekid263@gmail.com>
2019-11-09 16:53:55 -0800

    Tween fix, need rewrite on scrolling select text

--- Scripts/TweenScroll.gd
+++ Scripts/TweenScroll.gd
@@ -8,7 +8,7 @@ onready var SpriteNode = get_node("Sprite2")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	TweenNode.interpolate_property(SpriteNode, "transform/pos", Vector2(0,0), Vector2(300,300), 3, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	TweenNode.interpolate_property(SpriteNode, "global_position", Vector2(0,0), Vector2(300,300), 3, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
