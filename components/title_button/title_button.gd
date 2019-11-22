6c32c155a5b76538d2bbbd8796cf84f9ca85de4e
usaradark <gamekid263@gmail.com>
2019-11-22 02:49:35 -0800

    basic SE for title screen

---
+++ components/title_button/title_button.gd
@@ -0,0 +1,28 @@
+extends Button
+
+var HOVER = 0
+
+onready var hover = $ButtonHover
+onready var select = $ButtonSelect
+
+export(AudioStream) var button_pressed
+export(AudioStream) var button_hover
+
+func _ready():
+	select.set_stream(button_pressed)
+	hover.set_stream(button_hover)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _process(delta):
+	if is_hovered() and HOVER == 0:
+		hover.play()
+		HOVER = 1
+	if not is_hovered() and HOVER == 1:
+		HOVER = 0
+		
+func _on_pressed():
+	select.set_stream(button_pressed)
+	select.play()
--- components/title_buttons/title_buttons.gd
+++ components/title_buttons/title_buttons.gd
@@ -7,35 +7,38 @@ extends VBoxContainer
 onready var buttons = get_children()
 var selected
 
+signal new_game
+signal load_game
+signal options
+signal exit
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	buttons[0].grab_focus()
-	pass # Replace with function body.
-	
+	pass
+
+
 func _input(event):
-	if accepted_input(event):
-		selected = false
-		for child in buttons:
-			if child.has_focus():
-				selected = true
-				print(child.get_focus_previous())
-				break
-			
-			if !selected:
+	if event is InputEventKey:
+		if event.pressed:
+			if event.scancode == KEY_TAB:
+				for button in buttons:
+					if button.has_focus():
+						return
 				buttons[0].grab_focus()
-				print()
+				accept_event()
 
-func accepted_input(event):
-	if event.is_action_pressed("ui_up") or \
-		event.is_action_pressed("ui_down") or \
-		event.is_action_pressed("ui_left") or \
-		event.is_action_pressed("ui_right") or \
-		event.is_action_pressed("ui_focus_next") or \
-		event.is_action_pressed("ui_focus_prev"):
-		
-		return true
-	return false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_new_game_pressed():
+	emit_signal('new_game')
+
+
+func _on_load_game_pressed():
+	emit_signal('load_game')
+
+
+func _on_options_pressed():
+	emit_signal('options')
+
+
+func _on_exit_pressed():
+	emit_signal('exit')
