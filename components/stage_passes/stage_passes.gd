e7663b8cddcaf091855b696c7e53ad4f62ac7673
Annie <anniezhao107@gmail.com>
2019-11-26 00:35:23 -0800

    stage_passes ui now updates, expects 3 stages only

--- components/stage_passes/stage_passes.gd
+++ components/stage_passes/stage_passes.gd
@@ -7,13 +7,27 @@ var numStages		#Number of stages
 var passCount = 0	#Number of passes in level
 var failCount = 0	#Number of fails in level
 var failThreshold = float(1)/2	#Percentage of fails to trigger level fail
+var imageList = []	#Contains the texture nodes
+var image1
+var image2
+var image3
 
 signal game_over
 signal level_clear
 
+export(Array, Texture) var stateTextures
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	image1 = find_node("NinePatchRect")
+	image2 = find_node("NinePatchRect2")
+	image3 = find_node("NinePatchRect3")
+	imageList = [image1, image2, image3]
+	
+	_on_level_ready(3)
+	apply_pass()
+	apply_pass()
+	apply_fail()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -22,18 +36,25 @@ func _ready():
 func apply_pass():	#Stage passed
 	stageStates[currentStage] = 1
 	passCount += 1
+	imageList[currentStage].set_texture(stateTextures[1])
 	currentStage += 1
 	if currentStage == numStages:
+		print("level cleared")
 		emit_signal("level_clear")
 
 func apply_fail():	#Stage failed
 	stageStates[currentStage] = 0
 	failCount += 1
+	imageList[currentStage].set_texture(stateTextures[0])
 	currentStage += 1
 	if float(failCount)/numStages >= failThreshold:
+		print("game over")
 		emit_signal("game_over")
+	elif currentStage == numStages:
+		print("level cleared")
+		emit_signal("level_clear")
 
 func _on_level_ready(number):
 	numStages = number
 	for i in range(numStages):	#Init stageStates to hold -1's
-		stageStates[i] = -1
+		stageStates.append(-1)
