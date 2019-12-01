5d33fa9e88c1e62624e7ee8b6cff995744c2bfba
usaradark <gamekid263@gmail.com>
2019-11-30 16:52:39 -0800

    Fixed transition overlay to allow mouse to pass, reimported audio to not normalize, added end game screen

--- components/viewport_label/Viewport.gd
+++ components/viewport_label/Viewport.gd
@@ -3,6 +3,7 @@ extends Viewport
 onready var label = $Camera2D/current_word
 
 func _set_text(text):
+	#print('setting text: ', text)
 	label.set_size(Vector2(0, 0))
 	label.set_text(text)
-	set_size(label.get_size())
+	set_size(label.get_size() + Vector2(50, 0))
